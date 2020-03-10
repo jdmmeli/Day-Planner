84dac6c1e233dc7a75d07c49e441535ecbc21343
Jason Meli <jdmmeli@yahoo.com>
2020-03-10 13:26:23 -0400

    added moment.js, got date to display

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+    var dateFormat = moment().format('MMMM Do')
+    var dayFormat = moment().format('dddd')
+    $("#current-date").text(dayFormat + ", " + dateFormat)
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
