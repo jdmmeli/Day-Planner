f5ebbacc1f91391334bc88a19d488c842865ee1c
Jason Meli <jdmmeli@yahoo.com>
2020-03-13 16:39:17 -0400

    colors

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
 
-    var dateFormat = moment().format('MMMM Do')
-    var dayFormat = moment().format('dddd')
-    $("#current-date").text(dayFormat + ", " + dateFormat)
+
+    var time = ("HH:mm:ss");
+    var dateFormat = moment().format('MMMM Do');
+    var dayFormat = moment().format('dddd');
+    $("#current-date").text(dayFormat + ", " + dateFormat);
+    var time = ("HH:mm");
+
+    var colorsTime = setInterval(colorsTime, 1000);
+
+    $("#message").css("color" , "black");
+    $("#message").css("background-color" , "black");
+
+
+    // colors ()
 
 
     var niner = JSON.parse(localStorage.getItem("nineA"));
@@ -34,60 +45,186 @@ $(document).ready(function () {
 
 
     $("#nine").on("click", function () {
-        var nine = $("#9am").val();
+        var nine = $("#9am").val().trim();
         console.log(nine)
         localStorage.setItem("nineA", JSON.stringify(nine));
     })
 
     $("#ten").on("click", function () {
-        var ten = $("#10am").val();
+        var ten = $("#10am").val().trim();
         console.log(ten);
         localStorage.setItem("tenA", JSON.stringify(ten));
     })
 
     $("#eleven").on("click", function () {
-        var eleven = $("#11am").val();
+        var eleven = $("#11am").val().trim();
         console.log(eleven)
         localStorage.setItem("elevenA", JSON.stringify(eleven));
     })
 
     $("#twelve").on("click", function () {
-        var twelve = $("#12am").val();
+        var twelve = $("#12am").val().trim();
         console.log(twelve)
         localStorage.setItem("twelveP", JSON.stringify(twelve));
     })
 
     $("#one").on("click", function () {
-        var one = $("#1pm").val();
+        var one = $("#1pm").val().trim();
         console.log(one)
         localStorage.setItem("oneP", JSON.stringify(one));
     })
 
     $("#two").on("click", function () {
-        var two = $("#2pm").val();
+        var two = $("#2pm").val().trim();
         console.log(two)
         localStorage.setItem("twoP", JSON.stringify(two));
     })
 
     $("#three").on("click", function () {
-        var three = $("#3pm").val();
+        var three = $("#3pm").val().trim();
         console.log(three)
         localStorage.setItem("threeP", JSON.stringify(three));
     })
 
     $("#four").on("click", function () {
-        var four = $("#4pm").val();
+        var four = $("#4pm").val().trim();
         console.log(four)
         localStorage.setItem("fourP", JSON.stringify(four));
     })
 
     $("#five").on("click", function () {
-        var five = $("#5pm").val();
-        console.log(five)
+        var five = $("#5pm").val().trim();
+        console.log(five);
         localStorage.setItem("fiveP", JSON.stringify(five));
-    })
+    });
 
-    
+    colors ();
+        
+    function colors() {
+        if (moment().isAfter(moment("9:59:59", time))) {
+            $("#9am").css("background-color", "lightgray");
+        }
+
+        else if (moment().isBetween(moment("9:00:00", time), moment("9:59:59" , time))) {
+            $("#9am").css("background-color", "red");
+        }
+
+        else if (moment().isBefore(moment("8:59:59", time))) {
+            $("#9am").css("background-color", "green");
+        }
+
+        if (moment().isAfter(moment("10:59:59", time))) {
+            $("#10am").css("background-color", "lightgray");
+        }
+
+        else if (moment().isBetween(moment("10:00:00", time), moment("10:59:59" , time))) {
+            $("#10am").css("background-color", "red");
+        }
+
+        else if (moment().isBefore(moment("9:59:59", time))) {
+            $("#10am").css("background-color", "green");
+        }
+
+        if (moment().isAfter(moment("11:59:59", time))) {
+            $("#11am").css("background-color", "lightgray");
+        }
+
+        else if (moment().isBetween(moment("11:00:00", time), moment("11:59:59" , time))) {
+            $("#11am").css("background-color", "red");
+        }
+
+        else if (moment().isBefore(moment("10:59:59", time))) {
+            $("#11am").css("background-color", "green");
+        }
+
+        if (moment().isAfter(moment("12:59:59", time))) {
+            $("#12am").css("background-color", "lightgray");
+        }
+
+        else if (moment().isBetween(moment("12:00:00", time), moment("12:59:59" , time))) {
+            $("#12am").css("background-color", "red");
+        }
+
+        else if (moment().isBefore(moment("11:59:59", time))) {
+            $("#12am").css("background-color", "green");
+        }
+
+        if (moment().isAfter(moment("13:59:59", time))) {
+            $("#1pm").css("background-color", "lightgray");
+        }
+
+        else if (moment().isBetween(moment("13:00:00", time), moment("13:59:59" , time))) {
+            $("#1pm").css("background-color", "red");
+        }
+
+        else if (moment().isBefore(moment("12:59:59", time))) {
+            $("#1pm").css("background-color", "green");
+        }
+
+        if (moment().isAfter(moment("14:59:59", time))) {
+            $("#2pm").css("background-color", "lightgray");
+        }
+
+        else if (moment().isBetween(moment("14:00:00", time), moment("14:59:59" , time))) {
+            $("#2pm").css("background-color", "red");
+        }
+
+        else if (moment().isBefore(moment("13:59:59", time))) {
+            $("#2pm").css("background-color", "green");
+        }
+
+        if (moment().isAfter(moment("15:59:59", time))) {
+            $("#3pm").css("background-color", "lightgray");
+        }
+
+        else if (moment().isBetween(moment("15:00:00", time), moment("15:59:59" , time))) {
+            $("#3pm").css("background-color", "red");
+        }
+
+        else if (moment().isBefore(moment("14:59:59", time))) {
+            $("#3pm").css("background-color", "green");
+        }
+
+        if (moment().isAfter(moment("16:59:59", time))) {
+            $("#4pm").css("background-color", "lightgray");
+        }
+
+        else if (moment().isBetween(moment("16:00:00", time), moment("16:59:59" , time))) {
+            $("#4pm").css("background-color", "red");
+        }
+
+        else if (moment().isBefore(moment("15:59:59", time))) {
+            $("#4pm").css("background-color", "green");
+        }
+
+        if (moment().isAfter(moment("17:59:59", time))) {
+            $("#5pm").css("background-color", "lightgray");
+        }
+
+        else if (moment().isBetween(moment("17:00:00", time), moment("17:59:59" , time))) {
+            $("#5pm").css("background-color", "red");
+        }
+
+        else if (moment().isBefore(moment("16:59:59", time))) {
+            $("#5pm").css("background-color", "green");
+        }
+
+        
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
